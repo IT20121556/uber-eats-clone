2e83bb285180337babdbd1cf80aca41bed3adcfb
Arachchige P.D.A.M <it20121556@my.sliit.lk>
2021-11-17 18:19:11 +0530

    SearchBar_Added

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text, SafeAreaView, StyleSheet, StatusBar } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.view}>
         <HeaderTabs />
+        <SearchBar />
       </View>
     </SafeAreaView>
   );
