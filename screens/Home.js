361ad0159ff23399a3f374311c2cfa03dc381d52
Arachchige P.D.A.M <it20121556@my.sliit.lk>
2021-11-21 14:34:42 +0530

    Restaurant-Items_Added

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,54 @@
-import React from "react";
-import { View, Text, SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  ScrollView,
+} from "react-native";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
+import RestaurantItems, {
+  localRestaurants,
+} from "../components/RestaurantItems";
 import SearchBar from "../components/SearchBar";
 
-export default function Home() {
+const YELP_API_KEY =
+  "2Mkgq3LkbGyObKfatUyAk7Dh3-C_0blj6pvWvk4L1pdlWGMwnzeYrW2vct2a2kdNX9j-2bN7UIiv8FsXIslo-AQqNmqSoEK5XPnuwla7nXQNdyIxdLp-DmSe2qqWYXYx";
+
+export default function Home({ navigation }) {
+  const [restaurantData, setRestaurantData] = useState(localRestaurants);
+  const [city, setCity] = useState("San Francisco");
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => setRestaurantData(json.businesses));
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, [city]);
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.view}>
         <HeaderTabs />
-        <SearchBar />
+        <SearchBar cityHandler={setCity} />
       </View>
-      <Categories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems restaurantData={restaurantData} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
